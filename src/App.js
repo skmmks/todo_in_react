81522ab0376ef022d38f6c98e2f52a6d5e2f42df
Steve Min <stevekmin@gmail.com>
2019-11-12 03:59:57 +0000

    add styling to todo list create markcomplete method in todoitem component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
       {
         id: 2,
         title: 'Dinner with wife', 
-        completed: false
+        completed: true
       },
       {
         id: 3,
